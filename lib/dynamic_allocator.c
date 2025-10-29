b528a2c2e88d53b8b40b529bbbc8683ddd6b9050
shehab888 <shehabhussien888@gmail.com>
2025-10-29 18:40:42 +0300

    handle free block function

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -95,6 +95,128 @@ struct PageInfoElement* get_pageInfoArr_element_ptr(void* va){
 
 //===========================
 // 3) ALLOCATE BLOCK:
+//void *alloc_block(uint32 size)
+//{
+//	//==================================================================================
+//	//DON'T CHANGE THESE LINES==========================================================
+//	//==================================================================================
+//	{
+//		assert(size <= DYN_ALLOC_MAX_BLOCK_SIZE);
+//	}
+//	//==================================================================================
+//	//==================================================================================
+//	// RETURN NULL IF SIZE==0
+//	if (size == 0) return NULL;
+//
+//	// --- get nearest power-of-two block size >= size ---
+//	uint32 nearst_size = (1 << LOG2_MIN_SIZE);
+//	int index_of_nearst_size = 0; // index 0 corresponds to 1<<LOG2_MIN_SIZE
+//	while (nearst_size < size && (nearst_size << 1) <= DYN_ALLOC_MAX_BLOCK_SIZE) {
+//		nearst_size <<= 1;
+//		index_of_nearst_size++;
+//	}
+//	// now nearst_size is the smallest power-of-two >= size
+//	// index_of_nearst_size is the freeBlockLists index to use
+//
+//	uint32 numberOfFreeBlocks = LIST_SIZE(&freeBlockLists[index_of_nearst_size]);
+//	uint32 numberOfFreePages = LIST_SIZE(&freePagesList);
+//	struct BlockElement* needed_block = NULL;
+//
+//	// CASE 1: there is already a free block of this size
+//	if (numberOfFreeBlocks > 0) {
+//		cprintf("CASE 1\n");
+//		needed_block = LIST_FIRST(&freeBlockLists[index_of_nearst_size]);
+//		LIST_REMOVE(&freeBlockLists[index_of_nearst_size], needed_block);
+//
+//		// update page info (using index)
+//		uint32 pageInfoArr_index = get_pageInfoArr_index((void*)needed_block);
+//		if (pageInfoArr_index < NUM_PAGES) {
+//			if (pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks > 0)
+//				pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks--;
+//			else
+//				pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks = 0; // safety
+//		}
+//		return (void*)needed_block;
+//	}
+//
+//	// CASE 2: no free blocks of this size, but there is a free page to fill
+//	else if (numberOfFreeBlocks == 0 && numberOfFreePages > 0) {
+//		cprintf("CASE 2\n");
+//		// take a free page
+//		struct PageInfoElement* new_free_page = LIST_FIRST(&freePagesList);
+//		LIST_REMOVE(&freePagesList, new_free_page);
+//
+//		uint32 VA = to_page_va(new_free_page);
+//		void* ptr = (void*)VA;
+//		int ret = get_page(ptr); // reserve physical frame
+//
+//		if (ret != 0) {
+//			panic("CAN'T GET_PAGE OR ALLOC_PAGE IN ALLOC_BLOCK FUNCTION");
+//		}
+//
+//		// set page metadata BEFORE inserting blocks
+//		struct PageInfoElement* pageInfoElement = get_pageInfoArr_element_ptr((void*)VA);
+//		pageInfoElement->block_size = nearst_size;
+//		pageInfoElement->num_of_free_blocks = PAGE_SIZE / nearst_size; // initially all blocks free
+//
+//		// create blocks FOR THIS PAGE
+//		// IMPORTANT: insert new page's blocks at HEAD so that next LIST_FIRST returns block from this page
+//		uint32 blocks_per_page = PAGE_SIZE / nearst_size;
+//		for (uint32 i = 0; i < blocks_per_page; i++) {
+//			struct BlockElement* created_block = (struct BlockElement*)((uint8*)VA + (i * nearst_size));
+//			LIST_INSERT_HEAD(&freeBlockLists[index_of_nearst_size], created_block);
+//		}
+//
+//		// now allocate the first block from that list (which will be from the page we just populated)
+//		needed_block = LIST_FIRST(&freeBlockLists[index_of_nearst_size]);
+//		LIST_REMOVE(&freeBlockLists[index_of_nearst_size], needed_block);
+//
+//		// update pageInfo after actual removal
+//		if (pageInfoElement->num_of_free_blocks > 0)
+//			pageInfoElement->num_of_free_blocks--;
+//		else
+//			pageInfoElement->num_of_free_blocks = 0;
+//
+//		return (void*)needed_block;
+//	}
+//
+//	// CASE 3: no free blocks of this size AND no free pages of any kind
+//	else if (numberOfFreePages == 0) {
+//		cprintf("CASE 3\n");
+//		// try to get from larger block: request a block of double size and split it
+//		if (nearst_size <= DYN_ALLOC_MAX_BLOCK_SIZE / 2) {
+//			// request a block of next-higher size (recursive)
+//			void* bigger = alloc_block(nearst_size * 2);
+//			if (bigger == NULL) return NULL;
+//
+//			// split bigger block into two buddies of size = nearst_size
+//			// bigger points to start of the big block
+//			struct BlockElement* first_half = (struct BlockElement*)bigger;
+//			struct BlockElement* second_half = (struct BlockElement*)((uint8*)bigger + nearst_size);
+//
+//			// insert both halves into the free list of current size
+//			LIST_INSERT_HEAD(&freeBlockLists[index_of_nearst_size], second_half);
+//			LIST_INSERT_HEAD(&freeBlockLists[index_of_nearst_size], first_half);
+//
+//			// update page info for the page that contains 'bigger'
+//			uint32 pindex = get_pageInfoArr_index((void*)bigger);
+//			if (pindex < NUM_PAGES) {
+//				// bigger was taken from a page that previously had block_size = nearst_size*2
+//				// after split, that page should have block_size = nearst_size (we are now using smaller blocks)
+//				pageBlockInfoArr[pindex].block_size = nearst_size;
+//				pageBlockInfoArr[pindex].num_of_free_blocks = (PAGE_SIZE / nearst_size); // both halves included
+//			}
+//
+//			// now allocate from this size (should hit CASE1 now)
+//			return alloc_block(size);
+//		}
+//		// cannot split further
+//		return NULL;
+//	}
+//
+//	return (void*)needed_block;
+//}
+
 //===========================
 void *alloc_block(uint32 size)
 {
@@ -146,13 +268,13 @@ void *alloc_block(uint32 size)
 		LIST_REMOVE(&freeBlockLists[index_of_nearst_size],needed_block);
 
 		// FIRST WAY USING THE INDEX (UPDATE pageBlockInfoArr)
-		//uint32 pageInfoArr_index=get_pageInfoArr_index((void*)needed_block);
-		//pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks=pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks-1;
+		uint32 pageInfoArr_index=get_pageInfoArr_index((void*)needed_block);
+		pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks=pageBlockInfoArr[pageInfoArr_index].num_of_free_blocks-1;
 
 		// cprintf("66\n");
 		//SECOND WAY USING THE PTR  (UPDATE pageBlockInfoArr)
-		struct PageInfoElement* pageInfoElement=get_pageInfoArr_element_ptr((void*)needed_block);
-		pageInfoElement->num_of_free_blocks--;
+//		struct PageInfoElement* pageInfoElement=get_pageInfoArr_element_ptr((void*)needed_block);
+//		pageInfoElement->num_of_free_blocks--;
 
 		// cprintf("77\n");
 
@@ -167,42 +289,33 @@ void *alloc_block(uint32 size)
 		struct PageInfoElement* new_free_page=LIST_FIRST(&freePagesList);
 		LIST_REMOVE(&freePagesList,new_free_page);
 		// GET THE VA OF THE TAKEN FREE PAGE IN THE VM
-		// cprintf("11\n");
 		uint32 VA=to_page_va(new_free_page);
 		void* ptr=(void*)VA;
-		// cprintf("22\n");
-		int ret=get_page(ptr); // RESERVE PAGE IN TEH PHYSICAL MEM
-		// cprintf("33\n");
-		// CREATE BLOCKS IN THE FREE BLOCK LIST WHEN (GET PAGE SUCCESS , RET==0)
-		// cprintf("VA = %p\n", VA);
 
-		if(ret==0){
-			for(int i = 0; i < PAGE_SIZE / nearst_size; i++){
-				// cprintf("i = %d\n", i * nearst_size);
-				struct BlockElement* created_block = (struct BlockElement*)((uint32*)((uint8*)VA + (i * nearst_size)));// ADD THE OFFSET THAT REPRESTNT THE BLOCK SIZE OR NEARST_SIZE ON THE PAGE
-				LIST_INSERT_TAIL(&freeBlockLists[index_of_nearst_size],created_block);
-			}
-		}else {
+		int ret=get_page(ptr); // RESERVE PAGE IN TEH PHYSICAL MEM
+
+		if(ret!=0){
 			panic("CAN'T GET_PAGE OR ALLOC_PAGE IN ALLOC_BLOCK FUNCTION");
 		}
-		// for(int i = 0; i < 8; i++)
-		// {
-		// 	cprintf("size: %d number = %d\n", (1 << (i + 3)), freeBlockLists[i].size);
-		// }
-		// cprintf("GET BLOCKS SUCCESSFULLY\n");
 
-
-		// UPDATE THE CORRESPONDING PageBlockInfoArr ELEMENT
+		// INIT THE CORRESPONDING PageBlockInfoArr ELEMENT
 		struct PageInfoElement* pageInfoElement=get_pageInfoArr_element_ptr((void*)VA);
 		pageInfoElement->block_size=nearst_size;
-		pageInfoElement->num_of_free_blocks=(PAGE_SIZE/nearst_size)-1; // -1 for taken block
+		pageInfoElement->num_of_free_blocks=(PAGE_SIZE/nearst_size); // -1 for taken block
+
+		// CREATE BLOCKS IN THE FREE BLOCK LIST
+			for(int i = 0; i < PAGE_SIZE / nearst_size; i++){
+				// cprintf("i = %d\n", i * nearst_size);
+				struct BlockElement* created_block = (struct BlockElement*)((uint8*)VA + (i * nearst_size));// ADD THE OFFSET THAT REPRESTNT THE BLOCK SIZE OR NEARST_SIZE ON THE PAGE
+				LIST_INSERT_HEAD(&freeBlockLists[index_of_nearst_size],created_block);
+			}
 
 		// GET THE BLOCK AND RETURN IT
-		needed_block=LIST_LAST(&freeBlockLists[index_of_nearst_size]);
+		needed_block=LIST_FIRST(&freeBlockLists[index_of_nearst_size]);
 		LIST_REMOVE(&freeBlockLists[index_of_nearst_size],needed_block);
 
-//		pageInfoElement->num_of_free_blocks--;// -1 for taken block
-//		needed_block=alloc_block(nearst_size);
+		// UPDATE THE CORRESPONDING PageBlockInfoArr ELEMENT
+		pageInfoElement->num_of_free_blocks--;// -1 for taken block
 
 		return (void*)needed_block;
 
@@ -213,9 +326,6 @@ void *alloc_block(uint32 size)
 		if(nearst_size<=DYN_ALLOC_MAX_BLOCK_SIZE/2){
 			needed_block=alloc_block(nearst_size*2);
 		}
-//		else {
-//			panic("no free blocks and no free pages ");
-//		}
 		return (void*)needed_block;
 	}
 	return (void*)needed_block;
@@ -252,7 +362,7 @@ void free_block(void *va)
 	}
 	index_of_block_size-=LOG2_MIN_SIZE;
 	// REMVOE THE BLOCK FROM THE FREE BLOCK LIST
-	LIST_REMOVE(&freeBlockLists[index_of_block_size],(struct BlockElement*)va);
+	LIST_INSERT_HEAD(&freeBlockLists[index_of_block_size],(struct BlockElement*)va);
 
 	// UPDATE PageBlockInfoArr
 	struct PageInfoElement* pageInfoElement=get_pageInfoArr_element_ptr((void*)va);
