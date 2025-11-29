cbf1e516f738ca4f4d2d92415ddd095d68ce853e
shehab888 <shehabhussien888@gmail.com>
2025-11-29 16:43:53 +0200

    function ksem_wait and ksem_signal has been finished with their test =>  (((kernel module has been finished with their test)))

--- kern/conc/ksemaphore.c
+++ kern/conc/ksemaphore.c
@@ -25,8 +25,13 @@ void wait_ksemaphore(struct ksemaphore *ksem)
 	//TODO: [PROJECT'25.IM#5] KERNEL PROTECTION: #6 SEMAPHORE - wait_ksemaphore
 	//Your code is here
 	//Comment the following line
-	panic("wait_ksemaphore() is not implemented yet...!!");
-
+//	panic("wait_ksemaphore() is not implemented yet...!!");
+	acquire_kspinlock(&(ksem->lk));
+	ksem->count--;
+	if(ksem->count<0){
+		sleep(&(ksem->chan),&(ksem->lk));
+	}
+	release_kspinlock(&(ksem->lk));
 }
 
 void signal_ksemaphore(struct ksemaphore *ksem)
@@ -34,8 +39,13 @@ void signal_ksemaphore(struct ksemaphore *ksem)
 	//TODO: [PROJECT'25.IM#5] KERNEL PROTECTION: #7 SEMAPHORE - signal_ksemaphore
 	//Your code is here
 	//Comment the following line
-	panic("signal_ksemaphore() is not implemented yet...!!");
-
+//	panic("signal_ksemaphore() is not implemented yet...!!");
+		acquire_kspinlock(&(ksem->lk));
+		ksem->count++;
+		if(ksem->count<=0){
+			wakeup_one(&(ksem->chan));
+		}
+		release_kspinlock(&(ksem->lk));
 }
 
 
